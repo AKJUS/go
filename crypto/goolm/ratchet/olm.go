97fe7f43339852a2b3579999496cc4a7df768e9d
Tulir Asokan <tulir@maunium.net>
2026-04-12 18:14:03 +0300

    crypto/goolm: don't mutate state before successful decryption

--- crypto/goolm/ratchet/olm.go
+++ crypto/goolm/ratchet/olm.go
@@ -2,7 +2,6 @@
 package ratchet
 
 import (
-	"crypto/hmac"
 	"crypto/sha256"
 	"fmt"
 	"io"
@@ -118,7 +117,7 @@ func (r *Ratchet) Encrypt(plaintext []byte) ([]byte, error) {
 		r.SenderChains = *newSenderChain
 	}
 
-	messageKey := r.createMessageKeys(r.SenderChains.chainKey())
+	messageKey := r.SenderChains.chainKey().createMessageKeys()
 	r.SenderChains.advance()
 
 	cipher, err := aessha2.NewAESSHA2(messageKey.Key, olmKeysKDFInfo)
@@ -194,71 +193,81 @@ func (r *Ratchet) Decrypt(input []byte) ([]byte, error) {
 		}
 		return nil, fmt.Errorf("decrypt: %w", olm.ErrMessageKeyNotFound)
 	} else {
-		//Advancing the chain is done in this method
-		return r.decryptForExistingChain(receiverChainFromMessage, message, input)
+		receiverChainCopy := *receiverChainFromMessage
+		decrypted, skippedKeys, err := decryptForExistingChain(&receiverChainCopy, message, input)
+		if err != nil {
+			return nil, err
+		}
+		receiverChainFromMessage.CKey = receiverChainCopy.CKey
+		r.SkippedMessageKeys = append(r.SkippedMessageKeys, skippedKeys...)
+		if len(r.SkippedMessageKeys) > maxSkippedMessageKeys {
+			r.SkippedMessageKeys = r.SkippedMessageKeys[len(r.SkippedMessageKeys)-maxSkippedMessageKeys:]
+		}
+		return decrypted, nil
 	}
+}
+
+func (r *Ratchet) getNextRootKey(newRatchetKey crypto.Curve25519KeyPair, oldRatchetKey crypto.Curve25519PublicKey) (rootKey, newChainKey crypto.Curve25519PublicKey, err error) {
+	sharedSecret, err := newRatchetKey.SharedSecret(oldRatchetKey)
+	if err != nil {
+		return
+	}
+	derivedSecretsReader := hkdf.New(sha256.New, sharedSecret, r.RootKey, KdfInfo.Ratchet)
+	derivedSecrets := make([]byte, 2*sharedKeyLength)
+	if _, err = io.ReadFull(derivedSecretsReader, derivedSecrets); err != nil {
+		return
+	}
+	rootKey = derivedSecrets[:sharedKeyLength]
+	newChainKey = derivedSecrets[sharedKeyLength:]
+	return
 }
 
 // advanceRootKey created the next root key and returns the next chainKey
 func (r *Ratchet) advanceRootKey(newRatchetKey crypto.Curve25519KeyPair, oldRatchetKey crypto.Curve25519PublicKey) (crypto.Curve25519PublicKey, error) {
-	sharedSecret, err := newRatchetKey.SharedSecret(oldRatchetKey)
+	rootKey, chainKey, err := r.getNextRootKey(newRatchetKey, oldRatchetKey)
 	if err != nil {
 		return nil, err
 	}
-	derivedSecretsReader := hkdf.New(sha256.New, sharedSecret, r.RootKey, KdfInfo.Ratchet)
-	derivedSecrets := make([]byte, 2*sharedKeyLength)
-	if _, err := io.ReadFull(derivedSecretsReader, derivedSecrets); err != nil {
-		return nil, err
-	}
-	r.RootKey = derivedSecrets[:sharedKeyLength]
-	return derivedSecrets[sharedKeyLength:], nil
-}
-
-// createMessageKeys returns the messageKey derived from the chainKey
-func (r Ratchet) createMessageKeys(chainKey chainKey) messageKey {
-	hash := hmac.New(sha256.New, chainKey.Key)
-	hash.Write([]byte{messageKeySeed})
-	return messageKey{
-		Key:   hash.Sum(nil),
-		Index: chainKey.Index,
-	}
+	r.RootKey = rootKey
+	return chainKey, nil
 }
 
 // decryptForExistingChain returns the decrypted message by using the chain. The MAC of the rawMessage is verified.
-func (r *Ratchet) decryptForExistingChain(chain *receiverChain, message *message.Message, rawMessage []byte) ([]byte, error) {
+func decryptForExistingChain(chain *receiverChain, message *message.Message, rawMessage []byte) ([]byte, []skippedMessageKey, error) {
 	if message.Counter < chain.CKey.Index {
-		return nil, fmt.Errorf("decrypt: %w", olm.ErrChainTooHigh)
+		return nil, nil, fmt.Errorf("decrypt: %w", olm.ErrChainTooHigh)
 	}
 	// Limit the number of hashes we're prepared to compute
 	if message.Counter-chain.CKey.Index > maxMessageGap {
-		return nil, fmt.Errorf("decrypt from existing chain: %w", olm.ErrMsgIndexTooHigh)
+		return nil, nil, fmt.Errorf("decrypt from existing chain: %w", olm.ErrMsgIndexTooHigh)
 	}
+	skippedKeys := make([]skippedMessageKey, 0, min(maxSkippedMessageKeys, max(message.Counter-chain.CKey.Index, 0)))
 	for chain.CKey.Index < message.Counter {
-		messageKey := r.createMessageKeys(chain.chainKey())
-		skippedKey := skippedMessageKey{
-			MKey: messageKey,
-			RKey: chain.ratchetKey(),
+		if message.Counter-chain.CKey.Index < maxSkippedMessageKeys {
+			messageKey := chain.chainKey().createMessageKeys()
+			skippedKey := skippedMessageKey{
+				MKey: messageKey,
+				RKey: chain.ratchetKey(),
+			}
+			skippedKeys = append(skippedKeys, skippedKey)
 		}
-		r.SkippedMessageKeys = append(r.SkippedMessageKeys, skippedKey)
 		chain.advance()
 	}
-	if len(r.SkippedMessageKeys) > maxSkippedMessageKeys {
-		r.SkippedMessageKeys = r.SkippedMessageKeys[len(r.SkippedMessageKeys)-maxSkippedMessageKeys:]
-	}
-	messageKey := r.createMessageKeys(chain.chainKey())
+	messageKey := chain.chainKey().createMessageKeys()
 	chain.advance()
 	cipher, err := aessha2.NewAESSHA2(messageKey.Key, olmKeysKDFInfo)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	verified, err := message.VerifyMACInline(cipher, rawMessage)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	if !verified {
-		return nil, fmt.Errorf("decrypt from existing chain: %w", olm.ErrBadMAC)
+		return nil, nil, fmt.Errorf("decrypt from existing chain: %w", olm.ErrBadMAC)
 	}
-	return cipher.Decrypt(message.Ciphertext)
+	res, err := cipher.Decrypt(message.Ciphertext)
+	return res, skippedKeys, err
 }
 
 // decryptForNewChain returns the decrypted message by creating a new chain and advancing the root key.
@@ -273,24 +282,33 @@ func (r *Ratchet) decryptForNewChain(message *message.Message, rawMessage []byte
 		return nil, fmt.Errorf("decrypt for new chain: %w", olm.ErrMsgIndexTooHigh)
 	}
 
-	newChainKey, err := r.advanceRootKey(r.SenderChains.ratchetKey(), message.RatchetKey)
+	newRootKey, newChainKey, err := r.getNextRootKey(r.SenderChains.ratchetKey(), message.RatchetKey)
 	if err != nil {
 		return nil, err
 	}
 	newChain := newReceiverChain(newChainKey, message.RatchetKey)
-	r.ReceiverChains = append([]receiverChain{*newChain}, r.ReceiverChains...)
-	if len(r.ReceiverChains) > maxReceiverChains {
-		r.ReceiverChains = r.ReceiverChains[:maxReceiverChains]
-	}
 	/*
 		They have started using a new ephemeral ratchet key.
 		We needed to derive a new set of chain keys.
 		We can discard our previous ephemeral ratchet key.
 		We will generate a new key when we send the next message.
 	*/
-	r.SenderChains = senderChain{}
 
-	return r.decryptForExistingChain(&r.ReceiverChains[0], message, rawMessage)
+	decrypted, skippedKeys, err := decryptForExistingChain(newChain, message, rawMessage)
+	if err != nil {
+		return nil, err
+	}
+	r.RootKey = newRootKey
+	r.ReceiverChains = append([]receiverChain{*newChain}, r.ReceiverChains...)
+	if len(r.ReceiverChains) > maxReceiverChains {
+		r.ReceiverChains = r.ReceiverChains[:maxReceiverChains]
+	}
+	r.SenderChains = senderChain{}
+	r.SkippedMessageKeys = append(r.SkippedMessageKeys, skippedKeys...)
+	if len(r.SkippedMessageKeys) > maxSkippedMessageKeys {
+		r.SkippedMessageKeys = r.SkippedMessageKeys[len(r.SkippedMessageKeys)-maxSkippedMessageKeys:]
+	}
+	return decrypted, nil
 }
 
 // PickleAsJSON returns a ratchet as a base64 string encrypted using the supplied key. The unencrypted representation of the Account is in JSON format.
